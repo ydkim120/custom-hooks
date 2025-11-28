1dcde8860903cb96727e4cfd56e97bd8084edd46
dana.kim.skd <dana.kim.skd@mad.kabang.io>
2025-11-28 14:04:49 +0900

    feat - useScroll 추가

--- useScroll/useScroll.js
+++ useScroll/useScroll.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const useScroll = () => {
+export const useScroll = () => {
   const [state, setState] = useState({
     x: 0,
     y: 0,
@@ -14,14 +14,3 @@ const useScroll = () => {
   }, []);
   return state;
 };
-
-function App() {
-  const { y } = useScroll();
-  return (
-    <div className="App" style={{ height: "1000vh" }}>
-      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
-    </div>
-  );
-}
-
-export default App;
